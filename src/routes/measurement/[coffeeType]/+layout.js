d5aed4bbd901456091744a85f5085286ca2e9e07
Joshua Lonsako <jlo@Joshuas-MacBook-Pro.local>
2023-11-20 19:18:06 -0700

    Fixed layout bug

---
+++ src/routes/measurement/[coffeeType]/+layout.js
@@ -0,0 +1,10 @@
+import { coffeeData } from "$lib/coffeeData";
+
+export function load({ params }) {
+    let brewData = coffeeData.find((brewMethod) => brewMethod.route == params.coffeeType)
+    return {
+        brewData: {
+            name: brewData?.name,
+        }
+    }
+}
--- src/routes/measurement/[coffeeType]/+layout.svelte
+++ src/routes/measurement/[coffeeType]/+layout.svelte
@@ -1,21 +1,17 @@
 <script>
-    import { page } from "$app/stores"
     import { coffeeData } from "$lib/coffeeData";
     import PourOverIcon from "$lib/components/icons/PourOverIcon.svelte"
     import DownArrowIcon from "$lib/components/icons/DownArrowIcon.svelte";
 
-    let data = $page.url.pathname
-
-    let currentRoute = data.split("/")[2]
-
-    let brewName = coffeeData.filter((brewMethod) => brewMethod.route == currentRoute)[0].name
+    let {data} = $props()
+    let {name} = data.brewData
 
 </script>
 
 <button class="w-1/2 sm:w-1/3  md:w-1/4 lg:w-1/6 md:pt-5">
     <div class="flex flex-row py-1 text-slate-100 font-serif text-sm rounded-lg bg-zinc-700 justify-around place-items-center">
         <PourOverIcon />
-            {brewName}
+            {name}
         <DownArrowIcon />
     </div>
 </button>
