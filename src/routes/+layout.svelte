6617c88b0596d006224d5941d4ad73fde2f9d4e8
jtlonsako <jtlonsako.work@gmail.com>
2023-12-21 23:44:13 -0700

    Tried adding more icons and a logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
-    import "../app.css";
+    import MultiBrewLogo from "$lib/components/icons/MultiBrewLogo.svelte";
+import "../app.css";
 </script>
+<div class="absolute ml-1">
+  <a href="/">
+    <MultiBrewLogo />
+    </a>
+</div>
   <div class="grid grid-cols-1 justify-items-center">
     <slot />
   </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,24 @@
 <script>
     import PourOverIcon from "$lib/components/icons/PourOverIcon.svelte";
+    import VietnamesePhin from "$lib/components/icons/VietnamesePhin.svelte";
+    import FrenchPress from "$lib/components/icons/FrenchPress.svelte";
     import { coffeeData } from "$lib/coffeeData";
+
 </script>
 
-<div class=" mt-10 grid justify-items-center font-serif text-3xl">
+<div class=" mt-6 grid justify-items-center font-serif text-3xl">
     <p class="font-serif text-2xl text-white">Select Brew Type</p>
     <hr class="w-52 mx-auto opacity-30 rounded my-3">
-    {#each coffeeData as brewMethod}
-        <a href="/measurement/{brewMethod.route}" class="grid justify-center rounded-lg bg-zinc-700 hover:bg-zinc-500 my-2 w-60 h-48">
+    {#each coffeeData as brewMethod, i}
+        <a href="/measurement/{brewMethod.route}" class="grid justify-center justify-items-center rounded-lg bg-zinc-700 hover:bg-zinc-500 my-2 w-60 h-48">
             <div class="mt-4">
-                <PourOverIcon size="xl" />
+                {#if i == 0}
+                    <PourOverIcon size="xl" />                    
+                {:else if i == 1}
+                    <VietnamesePhin />
+                {:else}
+                    <FrenchPress />
+                {/if}
             </div>
             <p class="font-mono text-base text-zinc-50">{brewMethod.name}</p>
         </a> 
